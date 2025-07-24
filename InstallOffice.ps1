ff836adccccf7ea20fd73624ec3950e4f6ffdadc
m.jongeling@vandamict.nl <m.jongeling@vandamict.nl>
2025-07-24 13:32:32 +0200

    Installatie script Office

---
+++ InstallOffice.ps1
@@ -0,0 +1,58 @@
+# Onattended Office installatie via ODT
+
+# Pad naar ODT en config files
+$odtPath = "D:\ODT\setup.exe"
+$configDir = "D:\ODT\Configs"
+
+# Download ODT setup indien niet aanwezig
+if (!(Test-Path $odtPath)) {
+    Write-Host "ODT setup.exe niet gevonden, downloaden..."
+    $odtUrl = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_17531-20046.exe"
+    $odtInstaller = "D:\ODT\OfficeDeploymentTool.exe"
+    if (!(Test-Path "D:\ODT")) { New-Item -ItemType Directory -Path "D:\ODT" | Out-Null }
+    Invoke-WebRequest -Uri $odtUrl -OutFile $odtInstaller
+    Write-Host "Uitpakken van ODT setup..."
+    Start-Process -FilePath $odtInstaller -ArgumentList "/quiet /extract:D:\ODT" -Wait
+    Remove-Item $odtInstaller
+}
+
+# Definieer beschikbare configs in het script
+$configs = @{
+    "Office21" = "D:\ODT\Configs\Office21.xml"
+    "Office365"      = "D:\ODT\Configs\Office365.xml"
+}
+
+# Controleer of officeVersion is gezet
+if (-not $env:officeVersion) {
+    Write-Error "Omgevingsvariabele 'officeVersion' is niet gezet! Kies bijvoorbeeld 'Office2021' of 'Office365'."
+    exit 1
+}
+
+# Controleer of de gekozen versie bestaat in configs
+if (-not $configs.ContainsKey($env:officeVersion)) {
+    Write-Error "Ongeldige officeVersion: $env:officeVersion. Kies uit: $($configs.Keys -join ', ')"
+    exit 1
+}
+
+$configFile = $configs[$env:officeVersion]
+
+# Download config file indien niet aanwezig
+if (!(Test-Path $configFile)) {
+    # Controleer of de Configs map bestaat, maak aan indien nodig
+    if (!(Test-Path $configDir)) {
+        New-Item -ItemType Directory -Path $configDir | Out-Null
+    }
+    Write-Host "Config file $configFile niet gevonden, downloaden van GitHub..."
+    $githubUrl = "https://gist.githubusercontent.com/MelvinJongeling/4752223f0a3d63c8c561929abe33f3fa/raw/540e17b48d1524c6b7a3257b314e4ae86158e838/$env:officeVersion.xml"
+    Invoke-WebRequest -Uri $githubUrl -OutFile $configFile
+}
+
+if (!(Test-Path $configFile)) {
+    Write-Error "Config file $configFile niet gevonden of downloaden mislukt!"
+    exit 1
+}
+
+Write-Host "Start installatie van Office $env:officeVersion ..."
+Start-Process -FilePath $odtPath -ArgumentList "/configure `"$configFile`"" -Wait
+
+Write-Host "Installatie afgerond."
